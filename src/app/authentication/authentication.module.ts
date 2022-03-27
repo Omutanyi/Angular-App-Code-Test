bfc1cbef3d6ec009d616f7dc294c5437d649c290
Javier <javieromutanyi@gmail.com>
2022-03-28 00:50:06 +0300

    Auth forms fixed (Template Driven)

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { AuthenticationRoutingModule } from './authentication-routing.module';
 import { LoginComponent } from './components/login/login.component';
@@ -13,6 +14,7 @@ import { RegisterComponent } from './components/register/register.component';
   ],
   imports: [
     CommonModule,
+    FormsModule,
     AuthenticationRoutingModule
   ],
   exports: [
